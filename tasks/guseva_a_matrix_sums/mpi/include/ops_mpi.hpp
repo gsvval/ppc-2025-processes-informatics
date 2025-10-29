84ece8102f117ff45f04f6254d9ce898a51ec9b7
GssvvAl <gssvval@yandex.ru>
2025-10-29 18:51:38 +0000

    [upload]

---
+++ tasks/guseva_a_matrix_sums/mpi/include/ops_mpi.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "guseva_a_matrix_sums/common/include/common.hpp"
+#include "task/include/task.hpp"
+
+namespace guseva_a_matrix_sums {
+
+class GusevaAMatrixSumsMPI : public BaseTask {
+ public:
+  static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
+    return ppc::task::TypeOfTask::kMPI;
+  }
+  explicit GusevaAMatrixSumsMPI(const InType &in);
+
+ private:
+  bool ValidationImpl() override;
+  bool PreProcessingImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+
+  int rank_;
+};
+
+}  // namespace guseva_a_matrix_sums
